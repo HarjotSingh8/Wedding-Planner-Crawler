65da6834c7df1f987ccca130ab78769de9cd11b8
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-01-16 19:57:51 +0530

    script updated for mac

--- crawlerHyperthreaded/masterscriptmac.py
+++ crawlerHyperthreaded/masterscriptmac.py
@@ -1,55 +1,45 @@
 import os
 import json
-
-os.system('rm cities.json')
-os.system('scrapy crawl cityscraper -o cities.json')    #this will crawl for new celebrities
-#os.system('cp cities.json tutorial/spiders/cities.json')
-os.system('touch master.json')
-master= open("master.json","w+")
-master.write('{"banquets":{\r\n')
-master.close()
-#os.system('cat chandigarh.json >> master.json')
-cities=[]
-os.system("rm -rf cities")
-os.system("mkdir cities")
-with open('cities.json') as json_file:
-    data = json.load(json_file)
-    #print("data = ")
-    #print(data[0]['city'][0])
-    #print("this was data")
-    for c in data[0]['city'][:-1]:
-        #print('city: '+c)         #just verbose output for debugging purposes
-        #os.system('rm '+c+'.json')  #removing previous entries
-        script= open(c+".sh","w");
-        script.write('scrapy crawl banquetscrapermore -a start_url="https://weddingz.in/banquet-halls/'+c+'/all/" -o '+c+'.json\n')
-        script.write('mv '+c+'.json cities')
-        script.close();
-        os.system('bash '+c+'.sh &')
-        #os.system('gnome-terminal -e "scrapy crawl banquetscrapermore -a start_url="https://weddingz.in/banquet-halls/'+c+'/all/" -o '+c+'.json ; mv '+c+'.json cities"')
-    while True:
-        list = os.listdir("cities")
-        if len(list)==len(data[0]['city'])-1:
-            for c in data[0]['city'][:-1]:
-                os.system('rm '+c+'.sh')
-                master=open("master.json","a+")
-                master.write('"'+c+'":')
-                master.close()
-                os.system('cat cities/'+c+'.json >> master.json')
-                master=open("master.json","a+")
-                master.write('\r\n,\r\n')
-                master.close()
-                if os.stat('cities/'+c+'.json').st_size == 0:
-                    os.system("sed -i '' -e '$ d' master.json")
-                    os.system("sed -i '' -e '$ d' master.json")
-                else:
-                    cities.append(c)
-
-            break
-with open('cities.json', 'w') as outfile:
-    json.dump(cities, outfile)
-os.system("sed -i '' -e '$ d' master.json")
-master=open("master.json","a+")
-master.write('}}')
-master.close()
-#master.close()
-#os.system('scrapy crawl banquetscraper -o wed.json')
+import time
+import hashlib
+os.system('rm cities.json') #remove cities.json file (this step might be unnecessary but haven't checked yet)
+os.system('scrapy crawl cityscraper -o cities.json')    #this will crawl for new cities
+os.system('touch master.json')  #create master.json file to hold all data
+cities=[]   #cities list to store names of cities (to ignore destination wedding)
+checks=1
+os.system('python banquetcrawlermac.py &')
+with open('cities.json') as json_file:  #open data present in cities.json
+    data = json.load(json_file) #load data into a list using json library
+    for c in data[0]['city'][:-1]:  #running the cities present in the 'cities.json' file through a loop ignoring the last entry 'destination wedding' due to formatting issues
+        cities.append(c)
+dict={}
+dict['cities']=cities
+while checks>0: #this loop checks if all cities have been crawled
+    list = os.listdir("banquets") #just an arbitrary list to check if all the lists are completed
+    if len(list)==len(cities):   #check if banquet data from all cities has been recieved
+        checks=checks-1
+        print(checks)
+        dict['banquets'] = {}
+        count=0
+        for c in cities:  #run a loop on all cities to add their data to a single master.json file
+            dict['banquets'][c]= {}
+            os.system('rm '+c+'.sh')    #remove the script made for that city
+            if os.stat('banquets/'+c+'.json').st_size != 0:   #check if the <city>.json contains any data
+                with open('banquets/'+c+'.json') as json_file:  #open data present in cities.json
+                    data = json.load(json_file) #load data into a list using json library
+                    for d in data:  #running the cities present in the 'cities.json' file through a loop ignoring the last entry 'destination wedding' due to formatting issues
+                        count+=1
+                        if(count==1000):
+                            print("broke")
+                            break
+                        hash = hashlib.sha256(d["name"].encode('utf-8')).hexdigest()
+                        dict['banquets'][c][hash] = {}
+                        for e in d:
+                            dict['banquets'][c][hash][e]=d[e]
+                        #print(hashlib.sha256(d["name"].encode('utf-8')).hexdigest())
+    time.sleep(1)   #adds an interval after each check to reduce cpu usage
+print(cities)
+with open('cities.json', 'w') as outfile:   #open cities.json as writeable file (overwrite, not append)
+    json.dump(cities, outfile)  #write contents of cities converted to json format back to cities.json file
+with open('master.json', 'w') as outfile:   #open cities.json as writeable file (overwrite, not append)
+    json.dump(dict, outfile, indent=2)  #write contents of cities converted to json format back to cities.json file
